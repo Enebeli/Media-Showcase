0fdb4763e8634ba4cac30d8c1feac09a107711e9
Enebeli <enebelijahswill02@gmail.com>
2025-02-27 22:40:49 -0700

    JavaScript
    Added JavaScript code

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-document.addEventListener("click", () => {
-    
-})
+document.addEventListener("DOMContentLoaded", () => {
+    let filterSelect = document.getElementById("filter");
+    let movieList = document.querySelectorAll("figure");
+
+    filterSelect.addEventListener("change", () => {
+        let selectedGenre = filterSelect.value;
+
+        movieList.forEach((movie) => {
+            if (selectedGenre === "all" || movie.classList.contains(selectedGenre)) {
+                movie.style.display = "block";
+            } else {
+                movie.style.display = "none";
+            }
+        });
+    });
+});
